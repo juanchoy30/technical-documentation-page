66564f29567ca365e5bbd5e4aba1f7536417fddb
Juan Miguel Choy <juanchoy30@gmail.com>
2020-06-29 15:26:20 -0300

    dark theme button hidden jquery

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -1,8 +1,15 @@
-$("#dark-theme-button").click(function() {
-    $("#main-doc").addClass('dark-theme');
-    $("#navbar").addClass('dark-theme');
-  });
-  $("#light-theme-button").click(function() {
-    $("#main-doc").removeClass('dark-theme');
-    $("#navbar").removeClass('dark-theme');
-  });
+// This script makes the dark theme
+
+$("button#dark-theme-button").click(function() {
+  $("#main-doc").addClass('dark-theme');
+  $("#navbar").addClass('dark-theme');
+  $("button#light-theme-button").removeClass('hiddenFromPage');
+  $("button#dark-theme-button").addClass('hiddenFromPage');
+});
+
+$("button#light-theme-button").click(function() {
+  $("#main-doc").removeClass('dark-theme');
+  $("#navbar").removeClass('dark-theme');
+  $("button#dark-theme-button").removeClass('hiddenFromPage');
+  $("button#light-theme-button").addClass('hiddenFromPage');
+});
